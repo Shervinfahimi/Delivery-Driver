996f2f398aea3a6f5b180f6e9b01c7570fa3f0f3
Shervin <shervinfahimi2002@gmail.com>
2023-01-26 18:12:41 +0330

    Creat Speed Up spirite & set its scripts

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -6,14 +6,29 @@ public class Driver : MonoBehaviour
 {
     
     [SerializeField] float steertSpeed = 0.1f;
-    [SerializeField] float moveSpeed= 0.01f;
+    [SerializeField] float moveSpeed= 15f;
+    [SerializeField] float slowSpeed= 12f;
+    [SerializeField] float boostSpeed= 21f;
 
-    void Start()
+
+
+    //Slow down when player bump to anything 
+    void OnCollisionEnter2D(Collision2D other) 
     {
+        moveSpeed = slowSpeed;
         
     }
+     //Speed up when player past the yellow circle
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag=="Boost")
+        {
+            moveSpeed=boostSpeed;
+        }
 
-     
+    }
+
+    //Add inputs for car & fram rate independent 
     void Update()
     {
         float SteerAmount=Input.GetAxis("Horizontal")* steertSpeed * Time.deltaTime;
