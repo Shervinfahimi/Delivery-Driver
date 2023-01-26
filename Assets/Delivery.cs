31ad283d3ea6e52b6eaad68b7635f8d155267e19
Shervin <shervinfahimi2002@gmail.com>
2023-01-26 17:11:05 +0330

    Create Package&Customer Sprite & set their scripts

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+    [SerializeField] float destroyDelay = 1f;
+
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Oops!!");
         
     }
+
     void OnTriggerEnter2D(Collider2D other) 
     {
-        Debug.Log("wow!!");
+        if(other.tag=="Package" && !hasPackage)
+        {
+            Debug.Log("Package picked up");
+            hasPackage = true;
+            Destroy(other.gameObject,destroyDelay);
+        }
+
+
+        if(other.tag=="Customer" && hasPackage)
+        {
+            Debug.Log("Package deliverd  ");
+            hasPackage = false;
+        }
     }
 
 }
